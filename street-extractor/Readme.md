57bc2a5282fb3dcc213f913e447637fcf7c2d5e2
ztarbug <markus.zarbock@starwit.de>
2025-11-13 15:46:11 +0100

    add PostGIS exporter

--- street-extractor/Readme.md
+++ street-extractor/Readme.md
@@ -2,8 +2,15 @@
 This component downloads all street segments for a given city. It can then create CSV files with names and line strings as well as SQL imports for PostGIS.
 
 ## How to use
+
+Tools consists of two parts, extractor and PostGIS exporter. The first one extracts for a given city all streets and stores output as a gpkg file. PostGIS exporter creats SQL inserts and can import them into a PostGIS instance.
+
 ```bash
 poetry install
-poetry run python extractor.py
+# extractor
+poetry run python extractor.py "Wolfsburg, Germany"
+
+# PostGIS exporter
+poetry run python postgis_export.py Wolfsburg_Germany_street_segments.gpkg Wolfsburg db_name db_user db_password localhost 5432
 ```
 
--- street-extractor/postgis_export.py
+++ street-extractor/postgis_export.py
@@ -1,16 +1,15 @@
+import argparse
 import pandas as pd
 import geopandas as gpd
 from shapely import line_merge
 from shapely.ops import unary_union, linemerge
+from shapely.geometry import MultiLineString
 import psycopg2
 
 
 def generate_postgis_import(gdf, city_name):
-    result_query = ""
-    sql = """
-        INSERT INTO streets_catalog (city, street_name, street_path)
-        VALUES ('{city_name}', '{street_name}', ST_GeomFromText({geometry}, 4326))
-    """
+    result_query = []
+    sql = "INSERT INTO streets_catalog (city, street_name, street_path) VALUES ('{city_name}', '{street_name}', ST_SetSRID(ST_GeomFromText('{geometry}'),4326));"
     
     street_names = gdf['street_name'].unique()
     
@@ -23,28 +22,57 @@ def generate_postgis_import(gdf, city_name):
                 merged = segments[0]
                 continue
             merged = linemerge(unioned)
-            #print(segments)
+            result_query.append(sql.format(city_name=city_name, street_name=street_name, geometry=merged.wkt))
         except:
-            print("failed to merge " + street_name)
-            print(segments)
+            #print("failed to merge " + street_name)
+            geoSeries = gpd.GeoSeries(segments, crs="EPSG:4326")
+            multi = MultiLineString([geom for geom in geoSeries if geom is not None])
+            result_query.append(sql.format(city_name=city_name, street_name=street_name, geometry=multi.wkt))
             continue
-        #print(sql.format(city_name=city_name, street_name=street_name, geometry=merged.wkt))
 
-    
     return result_query
     
-    
-    
-def write_to_database(gdf, city_name, conn_params):
+def write_sql_to_file(sql, city_name):
+    with open(f"{city_name}_insert.sql", 'w') as f:
+        f.write("\n".join(sql))
+
+  
+def write_to_database(sql):
+    conn_params = {
+        "dbname": args.db_name,
+        "user": args.db_user,
+        "password": args.db_password,
+        "host": args.db_host,
+        "port": args.db_port
+    }
     conn = psycopg2.connect(**conn_params)
-    cur = conn.cursor()
-    
+    for query in sql:
+        cur = conn.cursor()
+        cur.execute(query)
+        conn.commit()
     pass
     
 def test_data_from_file():
     gdf = gpd.read_file("Wolfsburg_Germany_street_segments.gpkg")
-    generate_postgis_import(gdf, "Wolfsburg")
+    sql = generate_postgis_import(gdf, "Wolfsburg")
+    write_sql_to_file(sql, "Wolfsburg")
+    write_to_database(sql)
     return gdf
 
-if __name__ == '__main__':
+def main():
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('data_file', help='name of city e.g. Berlin, Germany')
+    arg_parser.add_argument('city', help='name of city e.g. Berlin, Germany')
+    arg_parser.add_argument('-s', '--sql', help='store generated sql inserts as file')
+    arg_parser.add_argument('db_name', help='postgres database name')
+    arg_parser.add_argument('db_user', help='postgres user name')
+    arg_parser.add_argument('db_password', help='postgres password')
+    arg_parser.add_argument('db_host', help='postgres host')
+    arg_parser.add_argument('db_port', help='postgres port')
+    global args
+    args = arg_parser.parse_args() 
     test_data_from_file()
+    
+
+if __name__ == '__main__':
+    main()
